8aa54651240a0d71dc764ac831820adcc14492ed
TylerD <tyler.domitrovich@concur.com>
2017-05-18 17:26:08 -0700

    added week days

--- new_file.rb
+++ new_file.rb
@@ -1,3 +1,6 @@
 puts "Hello, World!"
 puts "I know math: 1+1 = #{1+1}"
-puts "Phew wee"
+weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+weekdays.each do |day|
+    puts day
+end
